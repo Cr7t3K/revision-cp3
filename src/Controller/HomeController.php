fb39ee494737e88780f6b5d6337b9f0df7fead99
Guillaume <cvr.contact.pro@gmail.com>
2023-04-17 15:04:24 +0200

    Ajout form new

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,12 +9,10 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(ItemRepository $itemRepository): Response
     {
         $items = $itemRepository->findAll();
-
-
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -3,14 +3,39 @@
 namespace App\Controller;
 
 use App\Entity\Item;
+use App\Form\ItemFormType;
+use App\Form\ItemType;
 use App\Repository\ConfigurationRepository;
+use App\Repository\ItemRepository;
 use App\Service\TaxCalculator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ItemController extends AbstractController
 {
+    #[Route('/item/new', name: 'app_item_new')]
+    public function new(Request $request, ItemRepository $itemRepository): Response
+    {
+        $item = new Item();
+        $form = $this->createForm(ItemType::class, $item);
+
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $itemRepository->save($item, true);
+
+            $this->addFlash('success', 'Bienvenue sur mon site');
+
+        }
+
+        return $this->render('item/new.html.twig', [
+            'item' => $item,
+            'form' => $form->createView(),
+        ]);
+    }
+
     #[Route('/item/{id}', name: 'app_item_show')]
     public function show(
         Item $item,
