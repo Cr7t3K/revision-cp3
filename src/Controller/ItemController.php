e9772c342a00fa27eeb8667d36212960724ba592
Guillaume <cvr.contact.pro@gmail.com>
2023-04-17 16:09:49 +0200

    Création Auth + Access Control

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -12,22 +12,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class ItemController extends AbstractController
 {
     #[Route('/item/new', name: 'app_item_new')]
+    #[IsGranted('ROLE_ADMIN')]
     public function new(Request $request, ItemRepository $itemRepository): Response
     {
         $item = new Item();
         $form = $this->createForm(ItemType::class, $item);
 
-
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
             $itemRepository->save($item, true);
 
-            $this->addFlash('success', 'Bienvenue sur mon site');
+            $this->addFlash('success', 'Merci, un nouveau item a été créer');
 
+            return $this->redirectToRoute('app_home');
         }
 
         return $this->render('item/new.html.twig', [
@@ -37,6 +40,7 @@ class ItemController extends AbstractController
     }
 
     #[Route('/item/{id}', name: 'app_item_show')]
+    #[IsGranted('ROLE_USER')]
     public function show(
         Item $item,
         TaxCalculator $taxCalculator,
